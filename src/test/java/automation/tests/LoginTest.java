2343157f1f76742220053961db272f1a06ea7b3b
WhiAmI <glebpenza97@gmail.com>
2026-08-03 00:45:41 +0400

    add cardpage + one test

--- src/test/java/automation/tests/LoginTest.java
+++ src/test/java/automation/tests/LoginTest.java
@@ -1,26 +1,46 @@
 package automation.tests;
 
 import automation.base.BaseTest;
+import automation.pages.CartPage;
 import automation.pages.LoginPage;
 import automation.pages.ProductsPage;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class LoginTest extends BaseTest {
-    @Test
-    public void shouldLoginSuccessfully(){
+    private static final String PRODUCTS_TITLE = "Products";
+
+    private ProductsPage login(){
         LoginPage loginPage = new LoginPage(driver);
 
         loginPage.open();
         loginPage.enterUsername("standard_user");
         loginPage.enterPassword("secret_sauce");
 
-        ProductsPage productsPage = loginPage.clickLogin();
+        return loginPage.clickLogin();
+    }
+
+    @Test
+    public void shouldLoginSuccessfully(){
+
+        ProductsPage productsPage = login();
 
         String actualTitle = productsPage.getProductTitleText();
 
-        Assertions.assertEquals("Products", actualTitle);
+        Assertions.assertEquals(PRODUCTS_TITLE, actualTitle);
+    }
 
+    @Test
+    public void shouldAddProductToCart(){
+        ProductsPage productsPage = login();
+        productsPage.addFirstProductToCart();
 
+        CartPage cartPage = productsPage.goToCart();
+        String expectedTitle = cartPage.getTitle();
+
+        String expectedProductName = cartPage.getProductName();
+        Assertions.assertEquals("Sauce Labs Backpack", expectedProductName);
+
+        Assertions.assertEquals("Your Cart", expectedTitle);
     }
 }
