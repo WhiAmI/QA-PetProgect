ca74f0fcb4ccb39fbf763428d5307382eaecde57
Gleb <glebpenza97@gmail.com>
2026-08-19 17:17:31 +0300

    New Version Test Case

--- src/test/java/automation/pages/ProductsPage.java
+++ src/test/java/automation/pages/ProductsPage.java
@@ -3,14 +3,26 @@ package automation.pages;
 import automation.base.BasePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 public class ProductsPage extends BasePage {
+
     private final By productTitle = By.cssSelector(".title");
     private final By firstProductAddToCartButton = By.id("add-to-cart-sauce-labs-backpack");
     private final By cartButton = By.className("shopping_cart_link");
+    private final By sortList = By.className("product_sort_container");
+    private final By productPrices = By.className("inventory_item_price");
 
     public ProductsPage(WebDriver driver) {
         super(driver);
+        waitForVisibility(productTitle);
+    }
+
+    public boolean isOpened() {
+        return waitForVisibility(productTitle).isDisplayed();
     }
 
     public String getProductTitleText() {
@@ -24,5 +36,21 @@ public class ProductsPage extends BasePage {
     public CartPage goToCart() {
         click(cartButton);
         return new CartPage(driver);
+    }
+
+    public void sortByVisibleText(String text) {
+        Select select = new Select(waitForVisibility(sortList));
+        select.selectByVisibleText(text);
+    }
+
+    public List<Double> getProductPrices() {
+        waitForVisibility(productPrices);
+
+        return driver.findElements(productPrices)
+                .stream()
+                .map(WebElement::getText)
+                .map(price -> price.replace("$", ""))
+                .map(Double::parseDouble)
+                .toList();
     }
 }
--- src/test/java/automation/tests/ProductsTest.java
+++ src/test/java/automation/tests/ProductsTest.java
@@ -2,11 +2,25 @@ package automation.tests;
 
 import automation.base.BaseTest;
 import automation.pages.ProductsPage;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-public class ProductsPageTest extends BaseTest {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductsTest extends BaseTest {
+
+    @Test
     public void shouldSortProductsByPriceLowToHigh() {
         ProductsPage productsPage = login("standard_user", "secret_sauce");
 
-        productsPage.sortByVisibleText
+        productsPage.sortByVisibleText("Price (low to high)");
+
+        List<Double> actualPrices = productsPage.getProductPrices();
+
+        List<Double> expectedPrices = new ArrayList<>(actualPrices);
+        expectedPrices.sort(Double::compareTo);
+
+        Assertions.assertEquals(expectedPrices, actualPrices);
     }
 }
